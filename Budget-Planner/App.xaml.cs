810f27554bf45addd62dc23b63d7901adf30e72a
Curtis Smith <curtis.p.smith@zoho.com>
2023-09-27 19:19:22 +0100

    initial

---
+++ Budget-Planner/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Budget_Planner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Budget-Planner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Budget_Planner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
