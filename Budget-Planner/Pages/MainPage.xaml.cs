cae14123168eb066d8e34f2ab3904e491732e999
Curtis Smith <curtis.p.smith@zoho.com>
2023-09-28 22:21:42 +0100

    added in different entries for spending categories and began structuring the Today's Spending page

--- Budget-Planner/Pages/MainPage.xaml.cs
+++ Budget-Planner/Pages/MainPage.xaml.cs
@@ -2,13 +2,28 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
 
             CurrentDateTime.Text = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        public void CalculateTotalSpent(object sender, EventArgs e)
+        {
+            double _foodSpent;
+            double _billsSpent;
+            double _recreationSpent;
+            double _otherSpent;
+            double _totalSpent;
+
+            Double.TryParse(foodSpent.Text, out _foodSpent);
+            double.TryParse(billsSpent.Text, out _billsSpent);
+            double.TryParse(recreationalSpent.Text, out _recreationSpent);
+            double.TryParse(otherSpent.Text, out _otherSpent);
+
+            _totalSpent = _foodSpent + _billsSpent + _recreationSpent + _otherSpent;
+            totalSpent.Text = _totalSpent.ToString("C");
         }
 
         //private void OnCounterClicked(object sender, EventArgs e)
