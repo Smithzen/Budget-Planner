fa564c73f505338e3d469e9c76271fe7e973257a
Curtis Smith <curtis.p.smith@zoho.com>
2023-10-31 21:59:38 +0000

    slowly figuring out how to make a listView look how i want

--- Budget-Planner/pages/More.xaml.cs
+++ Budget-Planner/pages/More.xaml.cs
@@ -2,16 +2,45 @@ namespace Budget_Planner.pages;
 
 public partial class More : ContentPage
 {
-    public List<string> list = new List<string>();
+    public List<TestObject> list { get; set; } = new List<TestObject>();
 
 
-	public More()
+
+    public More()
 	{
 		InitializeComponent();
-        list.Add("hello");
-        list.Add("my");
-        list.Add("Name");
-        list.Add("is");
-        list.Add("greloy");
+
+        Int32 i = 0;
+        while(i < 10)
+        {
+            list.Add(new TestObject());
+            i++;
+        }
+
+    }
+}
+
+public class TestObject
+{
+    public string name { get; set; } = "Hello World";
+    public Int32 age { get; set; } = 1;
+}
+
+public class CustomViewCell : Microsoft.Maui.Controls.ViewCell
+{
+
+    public static readonly BindableProperty SelectedBackgroundColorProperty = BindableProperty.Create(
+        nameof(SelectedBackgroundColor), typeof(Color), typeof(CustomViewCell), Colors.White
+    );
+
+    public Color SelectedBackgroundColor
+    {
+        get { return (Color)GetValue(SelectedBackgroundColorProperty); }
+        set { SetValue(SelectedBackgroundColorProperty, value); }
+    }
+
+    public CustomViewCell()
+    {
+
     }
 }
