2ca1fdaa618952831cd8f8bfe265568dd9158373
Curtis Smith <curtis.p.smith@zoho.com>
2023-11-17 23:23:28 +0000

    added in some bits for adding an expense

--- Budget-Planner/pages/AddExpense.xaml.cs
+++ Budget-Planner/pages/AddExpense.xaml.cs
@@ -1,63 +1,44 @@
+using Budget_Planner.BudgetPlanner.Data;
 using MySqlConnector;
 using System.Diagnostics;
+using Budget_Planner.BudgetPlanner;
+using System.Collections.ObjectModel;
 
 namespace Budget_Planner.pages;
 
 public partial class AddExpense : ContentPage
 {
-    //string ConnectionString = "server=192.168.1.127;uid=myuser;pwd=mypass;database=budget_planner;";
-
-    //    AccountManager manager = (AccountManager)getSystemService(ACCOUNT_SERVICE);
-    //    Account[] list = manager.getAccounts();
-    //    String gmail = null;
-
-    //      for(Account account: list)
-    //      {
-    //          if(account.type.equalsIgnoreCase("com.google"))
-    //          {
-    //              gmail = account.name;
-    //              break;
-    //          }
-    //      }
-    // <uses-permission android:name="android.permission.GET_ACCOUNTS"></uses-permission>
-    //https://developer.android.com/training/permissions/requesting.html
-
-
+    public List<BPCategory> listCategories { get; set; } = new List<BPCategory>();
 
     public AddExpense()
 	{
         InitializeComponent();
+
+        GetSpendingCategoriesList();
+        BindingContext = this;
+
+    }
+
+    public void GetSpendingCategoriesList()
+    {
+        BPApplication bpApp = new BPApplication();
+        var result = bpApp.SpendingCategoriesGetAllCategories();
+        if (result.ServerResult)
+        {
+            listCategories = result.ServerResultDataList as List<BPCategory>;
+        }
+        else
+        {
+            ServerResult.Text = result.ServerResultMessage;
+            ServerResult.TextColor = Color.FromRgba(200, 0, 0, 1);
+            ServerResult.IsVisible = true;
+        }
+
     }
 
     public void AddNewExpense(object sender, EventArgs e)
     {
-        var builder = new MySqlConnectionStringBuilder
-        {
-            Server = "192.168.1.127",
-            UserID = "myuser",
-            Password = "mypass",
-            Database = "budget_planner",
-        };
 
-
-        MySqlConnection DBCon = new MySqlConnection(builder.ConnectionString);
-        try
-        {
-            DBCon.Open();
-
-            MySqlCommand cmd;
-            MySqlDataReader reader;
-
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine("AddNewExpense Ex: " + ex.Message);
-        }
-        finally
-        {
-            DBCon.Close();
-            DBCon.Dispose();
-        }
     }
 
 }
