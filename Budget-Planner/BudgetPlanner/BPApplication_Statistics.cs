c2c80a2f20dcdcebc70d7bb4c30220080edcf085
Curtis Smith <curtis.p.smith@zoho.com>
2023-11-26 22:02:43 +0000

    started working on charts for stats page

---
+++ Budget-Planner/BudgetPlanner/BPApplication_Statistics.cs
@@ -0,0 +1,142 @@
+﻿using Budget_Planner.BudgetPlanner.Data;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_Planner.BudgetPlanner
+{
+    public partial class BPApplication
+    {
+
+        public BPServerResult GetBarChartData(Int32 selectedIndex)
+        {
+            BPServerResult result = new BPServerResult();
+
+            List<BPExpense> listExpenses = new List<BPExpense>();
+            List<string> listCategoryNames = new List<string>();
+
+            MySqlConnection DBConRO = new MySqlConnection(builder.ConnectionString);
+            try
+            {
+
+                DBConRO.Open();
+
+                MySqlCommand cmd;
+                MySqlDataReader reader;
+
+                DateTime searchDate = GetSearchDate(selectedIndex);
+
+                cmd = new MySqlCommand("SELECT * FROM expenses WHERE ExpenseDate>=@ExpenseDate AND UserGUID=@UserGUID", DBConRO);
+                cmd.Parameters.Add(new MySqlParameter() { ParameterName = "@ExpenseDate", MySqlDbType = MySqlDbType.DateTime, Value = searchDate });
+                cmd.Parameters.Add(new MySqlParameter() { ParameterName = "@UserGUID", MySqlDbType = MySqlDbType.VarChar, Value = BPApplication.UserGUID });
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    //list of all retrieved expenses
+                    listExpenses.Add(new BPExpense()
+                    {
+                        ExpenseGUID = reader["ExpenseGUID"].ToString(),
+                        ExpenseCategory = new BPCategory() { CategoryName = reader["ExpenseCategoryName"].ToString() },
+                        ExpenseAmount = Convert.ToDouble(reader["ExpenseAmount"])
+
+                    });
+
+                    //adding unique categories to a list of categories
+                    if (!(listCategoryNames.Contains(reader["ExpenseCategoryName"].ToString())))
+                        listCategoryNames.Add(reader["ExpenseCategoryName"].ToString());
+                }
+                reader.Close();
+                reader.Dispose();
+
+                //list of lists by expense category
+                List<List<BPExpense>> listExpensesByCategory = new List<List<BPExpense>>();
+
+                //creating each unique list from the list of unique expense categories
+                foreach (string category in listCategoryNames)
+                {
+                    List<BPExpense> listExpenseCategory = new List<BPExpense>();
+                    listExpenseCategory = listExpenses.Where(p => p.ExpenseCategory.CategoryName == category).ToList();
+                    listExpensesByCategory.Add(listExpenseCategory);
+                }
+
+                result.ServerResultDataList = listExpensesByCategory;
+                result.ServerResult = true;
+                result.ServerResultMessage = "Successfully retrieved expenses";
+
+
+            }
+            catch (Exception ex)
+            {
+                result.ServerResult = false;
+                result.ServerResultMessage = "GetLineChartData Ex: " + ex.Message;
+            }
+            finally
+            {
+                DBConRO.Close();
+                DBConRO.Dispose();
+            }
+
+            return result;
+        }
+
+
+        public BPServerResult GetLineChartData(int selectedIndex)
+        {
+
+            BPServerResult result = new BPServerResult();
+
+
+            List<BPExpense> listExpenses = new List<BPExpense>();
+            List<DateTime> listExpenseDates = new List<DateTime>();
+
+
+            MySqlConnection DBConRO = new MySqlConnection(builder.ConnectionString);
+            try
+            {
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+            return result;
+        }
+
+
+        private static DateTime GetSearchDate(int selectedIndex)
+        {
+            DateTime searchDate = DateTime.Now;
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    searchDate = DateTime.Now.AddDays(-7);
+                    break;
+
+                case 1:
+                    searchDate = DateTime.Now.AddDays(-30);
+                    break;
+
+                case 2:
+                    searchDate = DateTime.MinValue;
+                    break;
+
+                default:
+                    throw new Exception("No valid date range added");
+            }
+
+            return searchDate;
+        }
+    }
+}
--- Budget-Planner/pages/Statistics.xaml.cs
+++ Budget-Planner/pages/Statistics.xaml.cs
@@ -1,9 +1,92 @@
+using Microcharts.Maui;
+using Microcharts;
+using SkiaSharp;
+using System.Security.Cryptography.X509Certificates;
+using Budget_Planner.BudgetPlanner;
+using Budget_Planner.BudgetPlanner.Data;
+
 namespace Budget_Planner.pages;
 
 public partial class Statistics : ContentPage
 {
+
+    public List<ChartEntry> listBarChartData = new List<ChartEntry>();
+
 	public Statistics()
 	{
 		InitializeComponent();
+
 	}
+
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        listBarChartData = new List<ChartEntry>();
+
+
+        pickerDateFilter.SelectedIndex = 0;
+
+
+
+        barChart.Chart = new BarChart
+        {
+            BackgroundColor = SKColor.Parse("#FFFFFF"),
+            Entries = listBarChartData,
+            LabelOrientation = Orientation.Horizontal,
+            YAxisPosition = Position.Left,
+            ShowYAxisText = true,
+            ShowYAxisLines = true,
+            ValueLabelOrientation = Orientation.Horizontal,
+            ValueLabelTextSize = 20,
+            LabelTextSize = 35,
+        };
+
+    }
+
+    private void pickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        getBarChartData();
+
+    }
+
+    private void getBarChartData()
+    {
+        BPApplication bpApp = new BPApplication();
+        var result = bpApp.GetBarChartData(pickerDateFilter.SelectedIndex);
+
+        foreach (List<BPExpense> listExpenseCategory in result.ServerResultDataList)
+        {
+            float value = 0;
+            string label = string.Empty;
+
+            //getting total value for all expenses in each category
+            foreach (BPExpense expense in listExpenseCategory)
+            {
+                if (string.IsNullOrEmpty(label))
+                {
+                    label = expense.ExpenseCategory.CategoryName;
+                }
+
+                value = value + (float)expense.ExpenseAmount;
+            }
+
+
+            var random = new Random();
+            var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+
+            //creating chart data
+            listBarChartData.Add(new ChartEntry(value)
+            {
+                Label = label,
+                ValueLabel = value.ToString("C"),
+                Color = SKColor.Parse(color)
+            });
+        }
+
+    }
+
+
 }
